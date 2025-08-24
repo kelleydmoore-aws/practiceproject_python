6db5e50d6300fcfa2a5d15cfc9b541f03b18aaa0
kmoore265s <kelleydmoore@gmail.com>
2025-08-24 06:46:00 -0500

    Practice Project Python

---
+++ python_script.py
@@ -0,0 +1,11 @@
+sentence = "We were never the champions!"
+
+print(sentence)
+
+print("We are the champions!")
+
+print("We are the champions!")
+
+print("We are the champions!")
+
+print("We are the champions!")
---
+++ python_script2.py
@@ -0,0 +1,24 @@
+n1 = 3
+n2 = 6
+
+n3 = 9
+n4 = 12
+
+n5 = 15
+n6 = 18
+
+def add_numbers(number1, number2):
+    number3 = None
+    number3 = number1 + number2
+    
+    return number3
+
+final_number = add_numbers(n1, n2)
+print(final_number)
+
+final_number = add_numbers(n3, n4)
+print(final_number)
+
+final_number = add_numbers(n5, n6)
+print(final_number)
+
